76878572a3bdccd5df15aa29d5faaed606b6ee87
Yugz29 <yannduzelier@gmail.com>
2026-01-19 21:22:28 +0100

    feat: add ProjectViewSet (CRUD, user-restricted)

--- workspace/views.py
+++ workspace/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Project
+from .serializers import ProjectSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
