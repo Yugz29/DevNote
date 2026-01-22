01327cc8f1740e7af72132bd956045287a3ccfc8
Yugz29 <yannduzelier@gmail.com>
2026-01-22 13:44:35 +0100

    config: add in-memory database for tests
    - Configure SQLite in-memory database for faster tests
    - Add TEST configuration in DATABASES setting
    - Improve test execution performance

--- devnote/settings.py
+++ devnote/settings.py
@@ -96,6 +96,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": ":memory:", # Tests will RAM (more faster)
+        }
     }
 }
 
