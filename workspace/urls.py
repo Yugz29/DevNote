c8a697ae69cebe05c8a1c8a6a66428d2de7257fb
Yugz29 <yannduzelier@gmail.com>
2026-01-28 20:56:51 +0100

    feat: add search endpoint route

--- workspace/urls.py
+++ workspace/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers as nested_routers
-from .views import ProjectViewSet, NoteViewSet, SnippetViewSet, TODOViewSet
+from .views import ProjectViewSet, NoteViewSet, SnippetViewSet, TODOViewSet, SearchView
 
 # Main router
 router = DefaultRouter()
@@ -23,4 +23,5 @@ projects_router.register(r'todos', TODOViewSet, basename='project-todos')
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(projects_router.urls)),
+    path('search/', SearchView.as_view(), name='search'),
 ]
